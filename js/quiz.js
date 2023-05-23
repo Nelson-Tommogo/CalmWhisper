720e917159a16ea322f5938155ec5e21b6b20fbe
Tommogo-coder <nelsontommogo9@gmail.com>
2023-05-23 16:32:49 +0300

    Mental Helath is Mandatory

--- js/quiz.js
+++ js/quiz.js
@@ -81,53 +81,54 @@ function clearStatusClass(element) {
 
 const questions = [
   {
-    question: 'Mental Health is',
+    question: 'What is Mental Health',
     answers: [
       { text: 'Absence of Mental Disorder', correct: false },
       { text: 'Important for specific people', correct: false },
-      { text: 'Cognitive, behavioral, and emotional well-being', correct: true },
+      { text: 'refers to a person emotional, psychological, and social well-being', correct: true },
       { text: 'Not related to well being of a person', correct: false }
     ]
   },
   {
-    question: 'Having mental health conditions means:',
+    question: 'What are some common signs of mental health problems?',
     answers: [
-      { text: 'Disturbances in a person\'s thinking, feeling, or behavior', correct: true },
+      { text: 'persistent feelings of sadness or hopelessness, significant changes in appetite or sleep patterns, withdrawal from activities or social interactions, extreme mood swings, difficulty concentrating or making decisions, excessive worry or anxiety, changes in energy levels, thoughts of self-harm or suicide, and unexplained physical ailments.'
+      , correct: true },
       { text: 'Low intelligence', correct: false },
       { text: 'Sign of weakness', correct: false },
       { text: 'Lack of Will-power', correct: false }
     ]
   },
   {
-    question: 'Mental Illness',
+    question: 'How can I support someone who is experiencing a mental health issue?',
     answers: [
-      { text: 'Can be treated! ', correct: true },
-      { text: 'Cannot be treated!', correct: false }
+      { text: 'providing non-judgmental and empathetic listening, offering reassurance and emotional support', correct: true },
+      { text: 'None', correct: false }
     ]
   },
   {
-    question: 'Poor mental health increases the risk for long-lasting (chronic) physical conditions like Heart Disease, Stroke & Cancer, etc.',
+    question: 'Are psychopaths mentally disabled?',
     answers: [
-      { text: 'Yes', correct: true },
-      { text: 'No', correct: false }
+      { text: 'No, psychopathy is not considered a mental disability.', correct: true },
+      { text: 'Yes', correct: false }
     ]
   },
   {
-    question: 'If you know someone of poor mental health:',
+    question: 'How can stress affect mental health?',
     answers: [
-      { text: 'Reaching out and letting them know that help is available', correct: false },
-      { text: 'Helping them access mental health services', correct: false },
-      { text: 'Learning and sharing facts, clear out myths if you are aware', correct: false },
-      { text: 'All of the above', correct: true }
+      { text: 'Stress does not affect anyone', correct: false },
+      { text: 'People are just born stressed', correct: false },
+      { text: 'Theres no stress', correct: false },
+      { text: ' It can contribute to the development or exacerbation of mental health conditions such as anxiety and depression. Prolonged stress can affect cognitive function, disrupt sleep patterns, impair decision-making abilities, and lead to physical symptoms such as headaches or stomachaches.', correct: true }
     ]
   },
   {
-    question: 'Mental health is not',
+    question: 'When should I seek professional help for mental health concerns?',
     answers: [
-      { text: 'A myth', correct: true },
-      { text: 'Normal as Physical health conditions', correct: false },
-      { text: 'Natural behavioral and emotional malfunction', correct: false },
-      { text: 'Something that needs attention and awareness', correct: false }
+      { text: 'It is important to seek professional help for mental health concerns when symptoms persist, worsen, or significantly interfere with daily functioning and well-being', correct: true },
+      { text: 'When am feeling well', correct: false },
+      { text: 'When am lonely', correct: false },
+      { text: 'When i need a girlfriend', correct: false }
     ]
   }
 ]
